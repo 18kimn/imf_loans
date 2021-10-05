c5b6b5942b75ac982bdf8cfa4001fb484993cc7a
Nathan <nathanckim18@gmail.com>
2021-10-05 01:39:57 -0400

    basic fixes for vite to stop warnings and get netlify to succeed build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import {resolve} from 'path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import visualizer from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: resolve(__dirname, 'dist/stats.html'),
+        }),
+      ],
+    },
+  },
 })
